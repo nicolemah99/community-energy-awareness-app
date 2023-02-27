3573a7b9b38daea67d24200f86386198abcde7fa
tuvag <tegranoien@alaska.edu>
2023-02-26 20:18:54 -0900

    Navbar functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "diesel-savings", to: "savings#index", as: :savings
 
   get "community-usage", to: "usage#index", as: :usage
+
+  get "about-page", to: "about#index", as: :about
 end
