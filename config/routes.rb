9edd459994b26d127cedf9b9472964b112b2750b
Nicole Mah <nicolemah99@gmail.com>
2023-02-26 18:25:26 -0900

    Added to Nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get "electricity-generation", to: "generation#index", as: :generation
 
-  get "diesel-savings" to: "savings#index", as: :savings
+  get "diesel-savings", to: "savings#index", as: :savings
 
-  get "community-usage" to "usage#index", as: :usage
+  get "community-usage", to: "usage#index", as: :usage
 end
