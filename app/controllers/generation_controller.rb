e8491a22dded7d39e95b420e63b18509f1459f74
Nicole Mah <nicolemah99@gmail.com>
2023-02-27 13:26:54 -0900

    showing values for instance

--- app/controllers/generation_controller.rb
+++ app/controllers/generation_controller.rb
@@ -4,7 +4,10 @@ class GenerationController < ApplicationController
     
     def index
         current_time = DateTime.now
-        cdt = current_time.strftime "%Y-%m-%d %H:00:00"
+        cdt = current_time.strftime "%2021-%m-%d %H:00:00"
+
+        @current = GenerationBreakdown.find_by dateTime: cdt
+
     end
     
 end
