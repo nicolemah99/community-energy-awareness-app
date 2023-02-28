da357aea91995d09d06c5cf502864776be54f4d6
Nicole Mah <nicolemah99@gmail.com>
2023-02-27 21:01:58 -0900

    Javascript Bubbles

--- app/controllers/generation_controller.rb
+++ app/controllers/generation_controller.rb
@@ -7,6 +7,10 @@ class GenerationController < ApplicationController
         cdt = current_time.strftime "%2021-%m-%d %H:00:00"
 
         @current = GenerationBreakdown.find_by dateTime: cdt
+        @solarP = @current.solarP * 100
+        @windP = @current.windP * 100
+        @renewP = @current.renewP * 100
+        @nonRenewP = @current.nonRenewP * 100
 
     end
     
