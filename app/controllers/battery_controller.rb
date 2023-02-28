395d022cb7cd56ac064399fad2b8a2edbd14088a
tuvag <tegranoien@alaska.edu>
2023-02-28 14:43:55 -0900

    battery state

--- app/controllers/battery_controller.rb
+++ app/controllers/battery_controller.rb
@@ -4,8 +4,17 @@ class BatteryController < ApplicationController
     def index
         current_time = DateTime.now
         cdt = current_time.strftime "%2021-%m-%d %H:00:00"
+        pdt = (current_time - (1.0/24)).strftime "%2021-%m-%d %H:00:00"
 
         @current = Battery.find_by timestamp: cdt
+        @past = Battery.find_by timestamp: pdt
         @charge = @current.charge
+        @chargep = @past.charge
+
+        if @chargep < @charge
+            @state = "Charging"
+        elsif @chargep > @charge
+            @state = "Discharging"
+        end
     end
 end
