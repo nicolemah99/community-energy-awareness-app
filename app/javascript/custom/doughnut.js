cc373840826104d7b2982e06ad4ef8186778264d
Nicole Mah <nicolemah99@gmail.com>
2023-03-15 12:42:51 -0800

    condsne doughnut.js

--- app/javascript/custom/doughnut.js
+++ app/javascript/custom/doughnut.js
@@ -1,51 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var windKW = this.getElementById("WindKW").value;
+  var windKW = this.getElementById("WindKW").value;
 	var solarKW = this.getElementById("SolarKW").value;
 	var dieselKW = this.getElementById("DieselKW").value;
 	var windP = this.getElementById("WindP").value;
 	var solarP = this.getElementById("SolarP").value;
 	var nRenewP = this.getElementById("NRenewP").value;
 	var renewP = this.getElementById("RenewP").value;
-    const ctx = document.getElementById('doughnutContainer');
-    const plugin = {
-  id: 'customCanvasBackgroundColor',
-  beforeDraw: (chart, args, options) => {
-    const {ctx} = chart;
-    ctx.save();
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.fillStyle = options.color || '#99ffff';
-    ctx.fillRect(0, 0, chart.width, chart.height);
-    ctx.restore();
-  }
-};
+  const ctx = document.getElementById('doughnutContainer');
+  const data = {
+    labels: ['Diesel', 'Wind', 'Solar'],
+    datasets: [{
+      label: 'kwh',
+      data: [dieselKW, windKW, solarKW],
+      backgroundColor: ['rgb(127,112,83)', 'rgb(54, 162, 235)','rgb(255, 205, 86)'],
+    }]
+  };
     
     new Chart(ctx, {
         type: 'doughnut',
-        data: {
-            labels: [
-    'Red',
-    'Blue',
-    'Yellow'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-        },
+        data: data,
         options: {
-            plugins: {
-                customCanvasBackgroundColor: {
-                    color: 'lightgreen'
-                }
+          responsive: true,
+          plugins: {
+            legend:{
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js'
             }
+          }
         },
-        plugins: [plugin],
     });
     
 });
