8d5048e259786d51c1411588137bec767f70aa1a
Nicole Mah <nicolemah99@gmail.com>
2023-04-06 20:30:44 -0800

    fix bugs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,19 @@
 require 'date'
 
 class MainController < ApplicationController
+
+    KWH_PER_GALLON = 13.00
+    DIESEL_PRICE = 5.00
     
     def index
         current_time = DateTime.now
         current_hour = current_time.strftime "%2021-%m-%d %H:00:00"
-        past_hour = (current_time - (1.0/24)).strftime "%2021-%m-%d %H:00:00"
+        past_hour = (Time.parse(current_hour) - 60 * 60).strftime "%2021-%m-%d %H:00:00"
+        @last_updated = current_time.strftime("%B #{current_time.day.ordinalize}, %Y at %l:00 %P AKST")
 
-        ## Generation Breakdown
+        ## Generation Breakdown Feature
+
+        #Query database for data from the current hour
         @current_record_breakdown = GenerationBreakdown.find_by dateTime: current_hour
         @total_kwh = @current_record_breakdown.total.round(1)
         @renew_kwh = @current_record_breakdown.renew.round(1)
@@ -15,23 +21,21 @@ class MainController < ApplicationController
         @wind_kwh = gon.wind_kwh = @current_record_breakdown.wind.round(1)
         @non_renew_kwh = @diesel_kwh = gon.diesel_kwh = @current_record_breakdown.nonRenew.round(1)
 
-        @renew_percent = (@renew_kwh / @total_kwh * 100).round(2)
-        @non_renew_percent = (@non_renew_kwh / @total_kwh * 100).round(2)
-        @solar_percent = gon.solar_percent = (@solar_kwh / @total_kwh * 100).round(2)
-        @wind_percent = gon.wind_percent = (@wind_kwh / @total_kwh * 100).round(2)
-        @diesel_percent = gon.diesel_percent = (@non_renew_kwh / @total_kwh * 100).round(2)
+        #Calculate percentage of each energy source
+        @renew_percent = calculatePercentage(@renew_kwh, @total_kwh)
+        @non_renew_percent = calculatePercentage(@non_renew_kwh, @total_kwh)
+        @solar_percent = gon.solar_percent = calculatePercentage(@solar_kwh, @total_kwh)
+        @wind_percent = gon.wind_percent = calculatePercentage(@wind_kwh, @total_kwh)
+        @diesel_percent = gon.diesel_percent = calculatePercentage(@non_renew_kwh, @total_kwh)
 
-        @curr_savings = @current_record_breakdown.year_total_non_renew
-        @last_updated = convertTime(@current_record_breakdown)
-        
-        ## Community Usage
+
+        ## Community Usage Feature
         start_of_day = current_time.strftime "%2021-%m-%d 00:00:00"
         end_of_day = current_time.strftime "%2021-%m-%d 23:00:00"
-        #can access in Javascript files
         gon.hourly_kwh_usage = GenerationBreakdown.where(dateTime: start_of_day..end_of_day).select(:total, :id).order(:id)
 
         
-        ## Battery Information
+        ## Battery Information Feature
         @current_record_battery = Battery.find_by timestamp: current_hour
         @past_hour_record_battery = Battery.find_by timestamp: past_hour
         current_charge = @current_record_battery.charge
@@ -45,52 +49,31 @@ class MainController < ApplicationController
             @battery_state = "Not charging"
         end
 
-        # 12-14 kwH per gallon
-        ## Diesel savings
-        kwh_per_gallon = 13.00
-        diesel_price = 5.00
-        @savings_date = []
+        ## Diesel Savings Feature
+        @savings_dates = []
         savings_date_and_amount = []
         collected_data = GenerationBreakdown.where(dateTime: "2021-01-01 00:00:00"..current_hour)
+        @curr_savings = @current_record_breakdown.year_total_non_renew
         
         # cycle through all dates
         collected_data.each do |record|
             # day and month 
             reading_time = record.dateTime.strftime("%e %b")
-            @savings_date.push reading_time
-            dollar_savings = (record.year_total_non_renew/kwh_per_gallon)*diesel_price
+            @savings_dates.push reading_time
+            dollar_savings = (record.year_total_non_renew / KWH_PER_GALLON) * DIESEL_PRICE
             savings_formatted = {:x => reading_time, :y => dollar_savings}
             savings_date_and_amount.push savings_formatted
 
         end
-        gon.completesavingdates = @savings_date
-        gon.completesavings = savings_date_and_amount
+        gon.complete_savings_dates = @savings_dates
+        gon.complete_savings = savings_date_and_amount
 
     end
 
 
-    #convert to 12hr clock
-    def convertTime(time) 
-        hour = time.dateTime.strftime("%H").to_i
-        if hour == 0
-            updatedHour = 12
-            meridiem = "am"
-
-        elsif hour < 12
-            updatedHour = hour.to_i
-            meridiem = "am"
-
-        elsif hour == 12
-            updatedHour = 12
-            meridiem = "pm"
-
-        elsif 12< hour and hour < 24
-            updatedHour = hour - 12
-            meridiem = "pm"
-        else
-            return "An error occured determining the time."
-        end
-        return time.dateTime.strftime("%B #{time.dateTime.day.ordinalize}, %Y at #{updatedHour}:00#{meridiem} AKST")
+    def calculatePercentage(numerator, denominator)
+        percent = ((numerator/denominator) * 100).round(2)
+        return percent
 
     end
 
