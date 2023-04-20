4091c085cdf199dfec80fc51fcd226d0310c99cf
Dayne Broderson <broderson@gmail.com>
2023-04-19 17:48:23 -0800

    YOLO seed load

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+require 'csv'
+
+batt_csv_text = File.read(Rails.root.join('seed-data', 'batteries.csv'))
+batt_csv = CSV.parse(batt_csv_text, headers:true)
+batt_csv.each do |row|
+  data = batt_csv.headers.zip(row).to_h
+  Battery.create!(data)
+end
+
+gen_csv_text = File.read(Rails.root.join('seed-data', 'generation_breakdowns.csv'))
+gen_csv = CSV.parse(gen_csv_text, headers:true)
+gen_csv.each do |row|
+  data = gen_csv.headers.zip(row).to_h
+  GenerationBreakdown.create!(data)
+end
